a44a264a872e3028709f22e1f30de2973762dbb0
Mnqobi Moyo <moyomnqobi36@gmail.com>
2026-07-29 12:33:52 +0200

    Configure project for Render deployment

--- nfc_website/settings.py
+++ nfc_website/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 # EMAIL CONFIGURATION
 
 
@@ -94,12 +95,10 @@ WSGI_APPLICATION = 'nfc_website.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
